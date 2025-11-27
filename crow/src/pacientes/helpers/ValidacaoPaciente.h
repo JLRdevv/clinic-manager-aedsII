30028ac08384ab5b6c91425e19d455a57c74bd66
unknown <joaolucas3406@gmail.com>
2025-11-26 23:32:14 -0300

    refactor(validação pacientes): melhora a validação adicionando mensagens de feedback

--- crow/src/pacientes/helpers/ValidacaoPaciente.h
+++ crow/src/pacientes/helpers/ValidacaoPaciente.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "crow.h"
+#include <variant>
 
 class ValidacaoPaciente
 {
 public:
-    static bool bodyCadastro(const crow::json::rvalue &json);
-    static bool bodyPatch(const crow::json::rvalue &json);
+    static std::variant<bool, std::string> bodyCadastro(const crow::json::rvalue &json);
+    static std::variant<bool, std::string> bodyPatch(const crow::json::rvalue &json);
 };
